ad67e2e8a8f84aa98db72bdb5c8c7ffd672a2db4
arpit <arpitec15@gmail.com>
2020-09-28 22:30:38 +0530

    commit

---
+++ DataStructures/src/arrays/EquilibriumPoint.java
@@ -0,0 +1,17 @@
+package arrays;
+
+//Find Equilibrium point where elements before & after it has equal sum
+
+public class EquilibriumPoint {
+
+	public static void main(String[] args) {
+		
+		int a[]	= {1,6,7,0,7};
+		
+		
+		
+		
+
+	}
+
+}
